37712e031cdb5ea08985ecc87f500cfa7fdc9423
n.mekh <nikita.mekh@gmail.com>
2020-05-07 13:40:57 +0300

    HGCMS-24

--- src/esas/cmsgate/hutkigrosh/utils/RequestParamsHutkigrosh.php
+++ src/esas/cmsgate/hutkigrosh/utils/RequestParamsHutkigrosh.php
@@ -16,8 +16,6 @@ class RequestParamsHutkigrosh extends RequestParams
     const BILL_ID = "bill_id";
     const PHONE = "phone";
     const PURCHASE_ID = "purchaseid"; // не менять, т.к. приходит с ХГ
-    const ORDER_NUMBER = "order_number";
-    const ORDER_ID = "order_id";
     const WEBPAY_STATUS = "webpay_status";
     const HUTKIGROSH_STATUS = "hutkigrosh_status";
 }
