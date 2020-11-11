c62a1ad2953b0fcf99b4377dab4557339513fa8c
n.mekh <ecvr813b>
2020-11-11 11:59:46 +0300

    CMSGATE-29: prestashop-hutkigrosh release

--- src/esas/cmsgate/hutkigrosh/PaysystemConnectorHutkigrosh.php
+++ src/esas/cmsgate/hutkigrosh/PaysystemConnectorHutkigrosh.php
@@ -42,7 +42,7 @@ class PaysystemConnectorHutkigrosh extends PaysystemConnector
     {
         return new PaySystemConnectorDescriptor(
             "cmsgate-hutkigrosh-lib",
-            new VersionDescriptor("v1.12.3", "2020-10-06"),
+            new VersionDescriptor("v1.13.1", "2020-11-11"),
             "Hutkigrosh (ERIP Belarus) cmsgate connector",
             "www.hutkigrosh.by",
             VendorDescriptor::esas(),
