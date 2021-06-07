e96d5a5193f490e0995852d4b5b4384a38355858
nikita.mekh <n.mekh@hutkigrosh.by>
2021-06-07 09:16:15 +0300

    CMSGATE-13: update to latest core version

--- src/esas/cmsgate/hutkigrosh/PaysystemConnectorHutkigrosh.php
+++ src/esas/cmsgate/hutkigrosh/PaysystemConnectorHutkigrosh.php
@@ -42,7 +42,7 @@ class PaysystemConnectorHutkigrosh extends PaysystemConnector
     {
         return new PaySystemConnectorDescriptor(
             "cmsgate-hutkigrosh-lib",
-            new VersionDescriptor("v1.13.9", "2021-02-12"),
+            new VersionDescriptor("v1.14.0", "2021-06-07"),
             "Hutkigrosh (ERIP Belarus) cmsgate connector",
             "www.hutkigrosh.by",
             VendorDescriptor::esas(),
